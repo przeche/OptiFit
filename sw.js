e2a90932d1af4d09183d38090ce5dee2d8f69b68
przeche <pchenski@gmail.com>
2026-06-23 20:55:36 +0200

    Add files via upload
    test version

---
+++ sw.js
@@ -0,0 +1,41 @@
+const CACHE_NAME = 'optifit-v1';
+const ASSETS = [
+  './eye_workout_dashboard.html',
+  './manifest.json',
+  'https://cdn.tailwindcss.com',
+  'https://unpkg.com/lucide@latest'
+];
+
+// Instalacja Service Workera i zapisanie plików w pamięci podręcznej (cache)
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      console.log('Cache zainicjalizowany.');
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+// Aktywacja i czyszczenie starego cache
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+});
+
+// Obsługa zapytań sieciowych (odczyt z cache, jeśli jesteśmy offline)
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(cachedResponse => {
+      return cachedResponse || fetch(event.request);
+    })
+  );
+});
